ada0921ade3fac9604a60c82a18fe0e7a21978b4
Pikeru <47680246+Pikeru@users.noreply.github.com>
2019-03-04 20:18:09 -0800

    SUBETE JODER!!!

---
+++ Entity.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    abstract class Entity
+    {
+        private int health;
+        private int mana;
+
+        public int Health
+        {
+            get { return health; }
+            set
+            {
+                if (value > 100)
+                {
+                    health = 100;
+                }
+                else if (value < 100)
+                {
+                    health = 0;
+                }
+                else
+                {
+                    health = value;
+                }
+            }
+        }
+        public int Mana
+        {
+            get { return mana; }
+            set
+            {
+                if (value > 100)
+                {
+                    mana = 100;
+                }
+                else if (value < 100)
+                {
+                    mana = 0;
+                }
+                else
+                {
+                    mana = value;
+                }
+            }
+        }
+
+        internal abstract void Muerte();
+        internal void Muerte1()
+        {
+            Console.WriteLine("Death . . .");
+        }
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    
+
+    
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int b, c, f=0;
+            Entity slayer = new Slayer();
+            Entity enemy = new Enemy();
+            Random rdn = new Random();
+
+            slayer.Health = 100;
+            enemy.Health = 100;
+            c = enemy.Health;
+            b = slayer.Health;
+            do
+            {
+                int a = rdn.Next(1, 10);
+                b=b-a;
+                Console.WriteLine(b);
+                int d = rdn.Next(1, 10);
+                c=c-d;
+                Console.WriteLine(c);
+                if(b<=0)
+                {
+                    f=f+1;
+                }
+                else if(c<=0)
+                {
+                    f++;
+                }
+                else
+                {
+                    Console.WriteLine("Mash Again");
+                }
+                Console.ReadKey();
+                
+            }
+            while (f<=0);
+
+            if(b<=0)
+            {
+                Console.WriteLine(b);
+                slayer.Muerte();
+            }
+            else
+            {
+                Console.WriteLine(c);
+                enemy.Muerte();
+            }
+            Console.ReadKey();
+        }
+    }
+}
